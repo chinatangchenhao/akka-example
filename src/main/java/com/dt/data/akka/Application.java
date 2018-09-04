788b4cd3316f5e8a21905f4b1d94953334037f40
tangchenhao <tangch@tangchenhaodeMacBook-Pro.local>
2018-09-04 17:55:17 +0800

    [update]

--- src/main/java/com/dt/data/akka/Application.java
+++ src/main/java/com/dt/data/akka/Application.java
@@ -2,6 +2,6 @@ package com.dt.data.akka;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello");  
+        System.out.println("hello akka");
     }
 }
